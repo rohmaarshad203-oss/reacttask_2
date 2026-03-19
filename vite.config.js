29edf668bb22c173b46788f5346a92c08f6f8eac
rohmaarshad203-oss <rohmaarshad203@gmail.com>
2026-03-20 03:42:03 +0500

    fixing base path for 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/reacttask_2/", 
 })
